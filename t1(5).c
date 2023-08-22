82a8a45a2cccb73a9bed16ce11b2a57f03bb940b
Fabrício Thomas <114196023+FabricioDD@users.noreply.github.com>
2023-08-22 13:56:58 -0300

    Add files via upload

--- t1(5).c
+++ t1(5).c
@@ -17,15 +17,10 @@ struct disciplina
 };
 typedef struct disciplina Disciplina;
 
-//Faça um programa que leia a quantidade de aulas ministradas em uma disciplina. Para esta disciplina
-//informada, considere 5 alunos. Para cada aluno, leia as duas notas obtidas pelo aluno na disciplina e o número de
-//aulas assistidas. A seguir, calcule e mostre a média final deste aluno e verifique se ele foi aprovado (status = ‘A’) ou
-//reprovado (status = ‘B’). Condição para ser aprovado: média >= 6 e mínimo de 75% de frequência.
-
 
 void leVetor (int tamanho, Aluno* vet){
     for (int i = 0; i < tamanho; ++i) {
-        printf("Digite a primeira nota do aluno %i:\n(m/f): ",i+1);
+        printf("Digite a primeira nota do aluno %i:\n ",i+1);
         scanf(" %f", &vet[i].nota1);
         printf("Digite a segunda nota do aluno %i:\n",i+1);
         scanf("%f", &vet[i].nota2);
@@ -35,29 +30,37 @@ void leVetor (int tamanho, Aluno* vet){
 }
 
 
-void aulasMinistradas(Disciplina dis){
-    printf("Digite a quantidade de aulas ministradas pelo professor:");
-    scanf("%i", &dis.aulasMinistradas);
+void aulasMinistradas(Disciplina* dis){
+    printf("Digite a quantidade de aulas ministradas pelo professor:\n");
+    scanf("%i", &dis->aulasMinistradas);
 }
 
 
+char* verificarAprovacao(char status) {
+    if (status == 'A') {
+        return "Aprovado";
+    } else if (status == 'B') {
+        return "Reprovado";
+    }
+}
+
 void avaliaMediaEPrinta(Disciplina dis){
-    float porcento= 0.75*dis.aulasMinistradas;
     for (int i = 0; i < 5; ++i) {
-        if (dis.vet[i].aulasAssistidas<=porcento){
+
+        if ((dis.aulasMinistradas/4)*3 > dis.vet[i].aulasAssistidas){
             dis.vet[i].status='B';
             dis.vet[i].media=(dis.vet[i].nota1 + dis.vet[i].nota2)/2;
-            printf("Aluno %i\nMedia final: %f\nSituacao: reprovado\n" ,i ,dis.vet[i].media);
+            printf("Aluno %i\nMedia final: %.2f\nSituacao: %s\n" ,i+1 ,dis.vet[i].media, verificarAprovacao(dis.vet[i].status));
             continue;
         }else{
             dis.vet[i].media=(dis.vet[i].nota1 + dis.vet[i].nota2)/2;
             if(dis.vet[i].media<6){
                 dis.vet[i].status='B';
-                printf("Aluno %i\nMedia final: %f\nSituacao: reprovado\n" ,i ,dis.vet[i].media);
+                printf("Aluno %i\nMedia final: %.2f\nSituacao: %s\n" ,i+1 ,dis.vet[i].media, verificarAprovacao(dis.vet[i].status));
                 continue;
             }else{
                 dis.vet[i].status='A';
-                printf("Aluno %i\nMedia final: %f\nSituacao: aprovado\n" ,i ,dis.vet[i].media);
+                printf("Aluno %i\nMedia final: %.2f\nSituacao: %s\n" ,i+1 ,dis.vet[i].media, verificarAprovacao(dis.vet[i].status));
                 continue;
             }
         }
@@ -66,7 +69,7 @@ void avaliaMediaEPrinta(Disciplina dis){
 
 int main(){
     Disciplina dis;
-    aulasMinistradas(dis);
+    aulasMinistradas(&dis);
     leVetor(5,dis.vet);
     avaliaMediaEPrinta(dis);
     return 0;
